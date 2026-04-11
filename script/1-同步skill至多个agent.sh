2d65e83c1c3d9f13bd61768561f06a2e925ace45
Victory-Hugo <giantlinlinlin@gmail.com>
2026-04-11 16:41:46 +0800

    添加cc-switch

--- script/1-同步skill至多个agent.sh
+++ script/1-同步skill至多个agent.sh
@@ -15,7 +15,8 @@ TARGET_DIRS=(
     "/home/luolintao/.codex/skills/"
     "/home/luolintao/.claude/skills/"
     "/home/luolintao/.copilot/skills/"
-    "/mnt/c/Users/Administrator/.codex/vendor_imports/skills"
+    "/home/luolintao/.cc-switch/skills/"
+    # "/mnt/c/Users/Administrator/.codex/vendor_imports/skills"
     # "/home/luolintao/.cursor/skills/"
     # 可以添加更多目标目录，例如：
 )
